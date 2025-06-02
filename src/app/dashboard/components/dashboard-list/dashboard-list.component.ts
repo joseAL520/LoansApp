fc9c75eb106e01663e5b018eccbe800f22896a92
joseAB <jose.albelt1817@gmail.com>
2025-06-02 08:19:45 -0500

    pipe-currrency

--- src/app/dashboard/components/dashboard-list/dashboard-list.component.ts
+++ src/app/dashboard/components/dashboard-list/dashboard-list.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, input, output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Client } from '../../interfaces/clients.interfaces';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-list',
-  imports: [RouterLink],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './dashboard-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
